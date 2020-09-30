2b76c0509116726a9c8b47fe55c794c8bf53d7a1
Nataliya R <n.rudenko@gmail.com>
2020-09-30 02:29:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function findMinAndRemoveSorted(array){
-  let min = array[0];
-  let minIndex = 0;
-  for (let i =0; i < array.length; i++){
-     if(array[0] < min){
-      min = array[i];
-      minIndex = i;
-     }
-    
+function findMinAndRemove(array){
+let min = array[0];
+let minIndex = 0;
+for(let i = 0; i < array.length; i++){
+  if(array[i] < min){
+    min = array[i];
+    minIndex = i;
   }
-  array.slice(minIndex, 1);
-  return min;
+  
+}
+array.splice(minIndex, 1);
+return min;
 }
